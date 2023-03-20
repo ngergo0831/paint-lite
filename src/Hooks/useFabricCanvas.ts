a7f5c3be7995697cd6e3c666055667e7e48695b3
Gergo Nagy <ngergo0831@gmail.com>
2023-03-20 23:25:05 +0100

    feat: add rectangle support

--- src/Hooks/useFabricCanvas.ts
+++ src/Hooks/useFabricCanvas.ts
@@ -10,26 +10,50 @@ interface FabricCanvasProps {
 export const useFabricCanvas = ({ fabricCanvas }: FabricCanvasProps) => {
   const { mode } = useContext(DrawModeContext);
 
-  const startAddingLine = (e: fabric.IEvent) => {
+  const startAddingShape = (e: fabric.IEvent) => {
     if (!fabricCanvas) return;
 
     const pointer = fabricCanvas.getPointer(e.e);
     const points = [pointer.x, pointer.y, pointer.x, pointer.y];
 
-    const line = new fabric.Line(points, {
-      fill: 'white',
-      stroke: 'white',
-      strokeWidth: 5,
-      perPixelTargetFind: true,
-      hasControls: false,
-      lockMovementX: true,
-      lockMovementY: true,
-      borderColor: 'transparent',
-      hoverCursor: 'default'
-    });
+    let shape: fabric.Line | fabric.Rect;
 
-    fabricCanvas.add(line);
-    fabricCanvas.setActiveObject(line);
+    switch (mode) {
+      case DrawMode.LINE:
+        shape = new fabric.Line(points, {
+          fill: 'white',
+          stroke: 'white',
+          strokeWidth: 5,
+          perPixelTargetFind: true,
+          hasControls: false,
+          lockMovementX: true,
+          lockMovementY: true,
+          borderColor: 'transparent',
+          hoverCursor: 'default'
+        });
+        break;
+      case DrawMode.RECTANGLE:
+        shape = new fabric.Rect({
+          left: pointer.x,
+          top: pointer.y,
+          width: 0,
+          height: 0,
+          hasControls: false,
+          lockMovementX: true,
+          lockMovementY: true,
+          perPixelTargetFind: true,
+          strokeWidth: 5,
+          stroke: 'white',
+          fill: 'transparent',
+          hoverCursor: 'default'
+        });
+        break;
+      default:
+        shape = new fabric.Object();
+    }
+
+    fabricCanvas.add(shape);
+    fabricCanvas.setActiveObject(shape);
     fabricCanvas.renderAll();
   };
 
@@ -39,10 +63,31 @@ export const useFabricCanvas = ({ fabricCanvas }: FabricCanvasProps) => {
     const pointer = fabricCanvas.getPointer(e.e);
     const activeObject = fabricCanvas.getActiveObject();
 
-    if (activeObject && activeObject.type === 'line') {
-      const line = activeObject as fabric.Line;
-      line.set({ x2: pointer.x, y2: pointer.y });
-      line.setCoords();
+    if (activeObject) {
+      switch (mode) {
+        case DrawMode.LINE:
+          const line = activeObject as fabric.Line;
+          line.set({ x2: pointer.x, y2: pointer.y });
+          line.setCoords();
+          break;
+        case DrawMode.RECTANGLE:
+          const rect = activeObject as fabric.Rect;
+
+          if (!rect.left) rect.left = 0;
+          if (!rect.top) rect.top = 0;
+
+          if (pointer.x > (rect.left ?? 0)) {
+            rect.set({ width: pointer.x - (rect.left ?? 0) });
+          }
+          if (pointer.y > (rect.top ?? 0)) {
+            rect.set({ height: pointer.y - (rect.top ?? 0) });
+          }
+
+          rect.setCoords();
+          break;
+        default:
+          break;
+      }
       fabricCanvas.renderAll();
     }
   };
@@ -51,9 +96,8 @@ export const useFabricCanvas = ({ fabricCanvas }: FabricCanvasProps) => {
     if (!fabricCanvas) return;
 
     const activeObject = fabricCanvas.getActiveObject();
-    if (activeObject && activeObject.type === 'line') {
-      const line = activeObject as fabric.Line;
-      line.setCoords();
+    if (activeObject) {
+      activeObject.setCoords();
     }
 
     fabricCanvas.discardActiveObject();
@@ -101,7 +145,7 @@ export const useFabricCanvas = ({ fabricCanvas }: FabricCanvasProps) => {
         obj.set('hoverCursor', 'default');
       });
 
-      fabricCanvas.on('mouse:down', startAddingLine);
+      fabricCanvas.on('mouse:down', startAddingShape);
       fabricCanvas.on('mouse:move', drawLine);
       fabricCanvas.on('mouse:up', finishDrawingLine);
 
@@ -109,16 +153,9 @@ export const useFabricCanvas = ({ fabricCanvas }: FabricCanvasProps) => {
     }
 
     return () => {
-      fabricCanvas.off('mouse:down', startAddingLine);
+      fabricCanvas.off('mouse:down', startAddingShape);
       fabricCanvas.off('mouse:move', drawLine);
       fabricCanvas.off('mouse:up', finishDrawingLine);
     };
   }, [fabricCanvas, mode]);
-
-  return {
-    startAddingLine,
-    drawLine,
-    finishDrawingLine,
-    handleDeleteObject
-  };
 };
